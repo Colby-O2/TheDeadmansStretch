71b7dd9e908f7b9fbc9fd313b51667a4b2d86eac
Colby-O2 <cokeefe919@gmail.com>
2026-04-08 19:17:03 -0400

    Camera Disable/Enable

--- Assets/Scripts/Runtime/UI/Views/PolaroidView.cs
+++ Assets/Scripts/Runtime/UI/Views/PolaroidView.cs
@@ -5,8 +5,26 @@ namespace ColbyO.Untitled
 {
     public class PolaroidView : View
     {
+        [SerializeField] private Camera _playerCamera;
+        [SerializeField] private Camera _polaroidCamera;
+
         public override void Init()
         {
+
+        }
+
+        public override void Show()
+        {
+            base.Show();
+            if (_polaroidCamera && !_polaroidCamera.gameObject.activeSelf) _polaroidCamera.gameObject.SetActive(true);
+            if (_playerCamera && _playerCamera.gameObject.activeSelf) _playerCamera.gameObject.SetActive(false);
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+            if (_polaroidCamera && _polaroidCamera.gameObject.activeSelf) _polaroidCamera.gameObject.SetActive(false);
+            if (_playerCamera && !_playerCamera.gameObject.activeSelf) _playerCamera.gameObject.SetActive(true);
         }
     }
 }
